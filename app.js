85ab22090d009282de319debaf925da43ea081a9
Eliezer14 <44449337+Eliezer14@users.noreply.github.com>
2022-12-01 16:51:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/estado',  (req, res, next) => {
+  res.send('No tienes acceso pero el servidor esta activo');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
