19b90157f2ed52a975befeded2eff51d497b7041
Eliezer14 <44449337+Eliezer14@users.noreply.github.com>
2022-12-01 21:41:22 -0400

    Add files via upload

--- app/main.js
+++ app/main.js
@@ -27,13 +27,16 @@ const requeridacantida = 10000;
 
 const cantida_hp = 10;
 
-const cantida_base = 1;
+const cantida_base = 10;
 
+const cantida_uso = 2;
 
 
 bot.onComment((data, responder)=> {
 
 	if (data.body.includes('!HUESO') && data.author != username) {
+		
+		let autovotos = 0;
 
 		let user = data.author;	
 		
@@ -45,11 +48,11 @@ bot.onComment((data, responder)=> {
 			
 			let balance = parseFloat(res.balance);
 				
-			let cantida = parseInt(balance / requeridacantida)
+			let cantida = parseInt(parseInt(balance / requeridacantida) * cantida_uso)
 				
 			if (cantida === 0) {
 					
-				responder.comment(`testeo del Bot Hueso, ${data.author} no tiene suficiente token HUESO, un uso por cada ${requeridacantida} token HUESO en tu cuenta`);
+				responder.comment(`testeo del Bot Hueso, ${data.author} no tiene suficiente token HUESO, ${cantida_uso} uso por cada ${requeridacantida} token HUESO en tu cuenta`);
 					
 				console.log(`El usuario ${user} no tiene hueso suficiente`)
 			} else {
@@ -60,8 +63,8 @@ bot.onComment((data, responder)=> {
 
 					const date_locate = new Date(date[0]).toLocaleDateString();
 		
-					let json = []
-
+					let json = 0;
+					
 					result.forEach((items) => {
 	
 						let op = items[1].op[1];
@@ -69,17 +72,25 @@ bot.onComment((data, responder)=> {
 						const fecha = new Date(items[1].timestamp).toLocaleDateString();
 	
 						if (items[1].op[0] === "comment" && op.author === user && (op.body).includes("!HUESO") && date_locate === fecha) {
-							json.push({'author': op.author, 'body': op.body, 'json_metadata': op.json_metadata, 'parent_author': op.parent_author, 'parent_permlink': op.parent_permlink, 'permlink': op.permlink, 'title': op.title,'timestamp': items[1].timestamp})
+							
+							json++
+							
+							if (op.parent_author === user) {
+								autovotos++
+							}
+							
 						}
 						
 					});
 					
-					console.log(`El usuario ${user} ha ejecutado ${json.length} veces el Comando !HUESO y tiene ${cantida} usos maximos por día`)
+					console.log(`El usuario ${user} ha ejecutado ${json} veces el Comando !HUESO y tiene ${cantida} usos maximos por día y se autovoto ${autovotos} veces `)
 					
-					let usos =  cantida - json.length;				
+					let usos =  cantida - json;				
 						
 					if (usos >= 0) {
+						
 						realizar_pago(usos)
+						
 					} else {
 						
 						responder.comment(`testeo del Bot Hueso, ${data.author} no tiene suficiente usos del comando !HUESO`);
@@ -94,8 +105,6 @@ bot.onComment((data, responder)=> {
 			
 			function realizar_pago(usos) {
 				
-				
-				
 				let pago = {
 				"contractName": "tokens",
 				"contractAction": "transfer",
@@ -106,16 +115,65 @@ bot.onComment((data, responder)=> {
 					"memo": ""
 					}
 				}
-				
-				responder.comment(`testeo del Bot Hueso, ${data.author} te a enviado ${cantida_base} token HUESO, te quedan ${usos} usos del comando !HUESO, pago ya hecho`);
-				
+
+				const commentario = `![1.png](https://files.peakd.com/file/peakd-hive/nahueldare3627/23wgKh4Rm5uw2BHNEvtU7VbJp45qU2im4Sbw4zZt1AayJpEV9WN1y3XeUaH26WKNGLvyD.png)
+
+<center>Se te han entregado 10 TOKEN HUESO
+Conoce el Proyecto Big Dog Bone [lee su White Paper aquí.](https://mundo-virtual.gitbook.io/untitled/)
+Si deseas ganar Tokens del Proyecto Big Dog Bone, usa en tus post las etiquetas #hueso y #mundovirtual
+Apoya nuestro trail de votación aquí [mv-curacion](https://hive.vote/dash.php?trail=mv-curacion&i=1)
+Por cada 10k de hueso en tu cartera puedes usar 2 veces al día el comando !HUESO en los comentarios de los post.</center>
+
+
+---
+
+
+<center>You have been given 10 TOKEN HUESO.
+Learn about the Big Dog Bone  Project [read their White Paper here.](https://mundo-virtual.gitbook.io/untitled/)
+If you want to win Big Dog Bone Project Tokens, use the hashtags #hueso and #mundovirtual in your posts.
+Support our voting trail here [mv-curation](https://hive.vote/dash.php?trail=mv-curacion&i=1)
+For every 10k of hueso in your wallet you can use 2 times a day the command !HUESO in the comments of the posts.</center>`
+
 				if (data.parent_author != username) {
 				
-				hive.broadcast.customJson(activeKey, [username], [], "ssc-mainnet-hive", JSON.stringify(pago))
+					if (user === data.parent_author) {
+						
+						/* repuesta de autovoto */
+						
+						if (autovotos <= 1) {	
+							
+						responder.comment(commentario)
+						
+						hive.broadcast.customJson(activeKey, [username], [], "ssc-mainnet-hive", JSON.stringify(pago))
 				
-				console.log(`El usuario ${user} tiene ${usos} usos, y se transfirieron ${cantida_base} HUESO a ${data.parent_author}`)
+				        console.log(`El usuario ${user} tiene ${usos} usos, y se transfirieron ${cantida_base} HUESO a ${data.parent_author}`)
+							
+							
+						} else {
+							
+							responder.comment(`testeo del Bot Hueso, no te puedes autovotar mas de una vez al día`);
+							
+							console.log(`El usuario ${user} tiene ${usos} usos, y alcanzo el limite de autovoto`)
+							
+						}
+							
+					} else {
+						
+						/* respuesta normal */
+						
+						responder.comment(commentario)
+						
+						hive.broadcast.customJson(activeKey, [username], [], "ssc-mainnet-hive", JSON.stringify(pago))
 				
-				} else {
+				        console.log(`El usuario ${user} tiene ${usos} usos, y se transfirieron ${cantida_base} HUESO a ${data.parent_author}`)
+						
+					}
+
+				} else {		
+					
+					/* respuesta si se vota al bot */
+					
+					responder.comment(commentario)
 					
 					pago.contractPayload.to = 'eliezer65'
 					
@@ -123,11 +181,12 @@ bot.onComment((data, responder)=> {
 					
 					console.log(`El usuario ${user} tiene ${usos} usos, y se transfirieron ${cantida_base} HUESO a ${pago.contractPayload.to}`)
 					
-				}
-				
-				
-				
+				}	
 			}
+		} else {
+			
+			console.log(err)
+			
 		}
 	});		
 		
